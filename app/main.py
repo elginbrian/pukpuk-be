3e6112ae4b834c31eb6448498a16d0ce1ba885f7
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2025-11-27 12:48:33 +0700

    fix: update cors policy

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://pukpuk-id.vercel.app", "http://localhost:8000"],  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
